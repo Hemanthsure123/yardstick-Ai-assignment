01298ff51209f15bb4eb22bee30bb233b3e26477
Hemanthsure123 <hemanthsure3@gmail.com>
2025-04-16 13:47:24 +0530

    abcdefghijklm

---
+++ backend/README.md
@@ -0,0 +1,1 @@
+# yardstickai-backend
--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.DB_CONNECTION).then(() => {
 })
 
 // start server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
